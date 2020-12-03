9dcb3b3455cde095cd80d40ac6d1bf9cc64eaaa3
Rocio Batres <jonasgirl21@gmail.com>
2020-12-03 01:10:37 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,8 @@
 require_relative "../lib/student.rb"
 
-  namespace :db do
-  desc 'migrate changes to your database'
-  task :migrate => :environment do
-    task :environment do
-  require_relative './config/environment'
-end
-    Student.create_table
-  end
-end
-
-namespace :db do
- 
-  ...
- 
-  desc 'seed the database with some dummy data'
-  task :seed do
-    require_relative './db/seeds.rb'
-  end
-end
 
 Student.create(name: "Melissa", grade: "10th")
 Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
-
-desc 'drop into the Pry console'
-task :console => :environment do
-  Pry.start
-end
